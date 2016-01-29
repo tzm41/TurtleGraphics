75fe1d4e8de9ad15ae6619765c446a960bfe8dcb
xZant <rafaeljr1995@gmail.com>
2016-01-29 17:46:05 -0500

    fixed lower case bug

--- TurtleGraphics.java
+++ TurtleGraphics.java
@@ -11,6 +11,7 @@
 import acm.program.*;
 
 public class TurtleGraphics extends GraphicsProgram {
+
 	/* Constants to set the application size */
 	public static final int APPLICATION_WIDTH = 1000;
 	public static final int APPLICATION_HEIGHT = 600;
@@ -33,23 +34,19 @@ public class TurtleGraphics extends GraphicsProgram {
 	 * command consists of a single letter, optionally followed by an integer.
 	 * The commands you must implement for this assignment are
 	 *
-	 * F# Moves forward the specified number of pixels (default = 50) 
-	 * L# Turn left the specified number of degrees (default = 90) 
-	 * R# Turn right the specified number of degrees (default = 90) 
-	 * U Raise the pen so that moving no longer draws a line 
-	 * D Lower the pen to resume line drawing 
-	 * X#{cmds} Execute the block of commands the specified number of times
+	 * F# Moves forward the specified number of pixels (default = 50) L# Turn
+	 * left the specified number of degrees (default = 90) R# Turn right the
+	 * specified number of degrees (default = 90) U Raise the pen so that moving
+	 * no longer draws a line D Lower the pen to resume line drawing X#{cmds}
+	 * Execute the block of commands the specified number of times
 	 */
 	public void execute(String str) {
 		// Fill this in
 		TurtleTokenizer turtleTok = new TurtleTokenizer(str);
-
 		while (turtleTok.hasMoreTokens()) {
-			
-			// get lower case so that later we only have to check the lower case version of the command
-			String currentToken = turtleTok.nextToken().toLowerCase();
-	
-			char command = currentToken.charAt(0);
+
+			String currentToken = turtleTok.nextToken();
+			char command = currentToken.charAt(0); // get letter command
 			String dx = currentToken.substring(1);
 
 			// identify command and run it
@@ -60,40 +57,40 @@ public class TurtleGraphics extends GraphicsProgram {
 					turtle.forward(Double.parseDouble(dx));
 
 			} else if (command == 'l') {
-				
+
 				if (dx.equals(""))
 					turtle.left(90);
 				else
 					turtle.left(Double.parseDouble(dx));
-				
+
 			} else if (command == 'r') {
 
 				if (dx.equals(""))
 					turtle.right(90);
 				else
 					turtle.right(Double.parseDouble(dx));
-				
+
 			} else if (command == 'u') {
-				turtle.penUp();	
+				turtle.penUp();
 			} else if (command == 'd') {
 				turtle.penDown();
 			} else if (command == 'x') {
-				// check for brackets
+
+				// check for braces
 				int repeats = Integer.parseInt(dx);
 				String temp = turtleTok.nextToken();
-				String nextToken = temp.substring(1, temp.length()-1); // get rid of brackets
-				
+				// get rid of braces
+				String nextToken = temp.substring(1, temp.length() - 1);
+
 				// recursively evaluate all the repeats
-				for (int i = 0; i < repeats; i++){
+				for (int i = 0; i < repeats; i++) {
 					execute(nextToken);
 				}
-				
-			}else
+
+			} else
 				continue; // invalid command
 		}
-
 	}
-	
 
 	/*
 	 * Implements the Replace button action, which replaces all instances of a
@@ -104,8 +101,8 @@ public class TurtleGraphics extends GraphicsProgram {
 	public void replaceAction() {
 		String text = ui.getProgramText();
 		String replace = ui.getReplacementField();
-        String[] strings = replace.split("\\s+?->\\s+?");
-        ui.setProgramText(text.replaceAll(strings[0], strings[1]));
+		String[] strings = replace.split("\\s+?->\\s+?");
+		ui.setProgramText(text.replaceAll(strings[0], strings[1]));
 	}
 
 	/* Private instance variables */
